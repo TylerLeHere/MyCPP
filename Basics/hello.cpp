1ef72c3927550b19683cdab758902f66976269cd
Tyler Le <tyler0308gm@gmail.com>
2026-01-11 20:26:39 -0800

    over the basics

---
+++ Basics/hello.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Tyler Le on 2026-01-10.
+//
+
+#include <cstring>
+#include <iostream>
+#include <ostream>
+
+int main() {
+    char s[] = "Hello";
+    std::cout << strlen(s) << std::endl;
+}
